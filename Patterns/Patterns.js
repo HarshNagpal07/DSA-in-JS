5e67c535c29ac3d107aa55f05a44b0fb9eb4fb7f
Harsh056-star <hnagpal056@gmail.com>
2025-07-14 23:45:53 +0530

    DSA

--- Patterns/Patterns.js
+++ Patterns/Patterns.js
@@ -8,7 +8,7 @@ console.log("world");*/
 
 // process.stdout.write("hello");
 // process.stdout.write("world");
-/*This will print the output in terminal*/
+/*This will print the output in terminal in the same line*/
 
 //  let a =prompt("Enter a number");
 //  console.log(a);// This will print in live server
@@ -59,4 +59,36 @@ Output=> Enter a number5
          * * * * 
          * * * * *
 */
-
+//Pattern 4 =>
+/*let prompt = require("prompt-sync")();
+let n = prompt("Enter a number");
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(j+" ");
+    }
+    console.log();
+}
+Output=>Enter a number5
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
+*/
+//Pattern 5 =>
+/*let prompt = require("prompt-sync")();
+let n = prompt("Enter a number");
+let count =1;
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(count + " ");
+        count = count + 1;
+    }
+    console.log();
+}
+Output=>Enter a number5
+1 
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+*/
