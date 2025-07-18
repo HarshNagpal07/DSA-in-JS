f2d512885de5240713147ba0d2a0edc6d910bf44
Harsh056-star <hnagpal056@gmail.com>
2025-07-18 13:31:04 +0530

    update

--- Array/script.js
+++ Array/script.js
@@ -127,7 +127,7 @@ while(i!=j){
 console.log(arr);*/
 
 //Question=> 5
-let arr = [1, 0, 0, 1, 0, 1, 0, 1, 1];
+/*let arr = [1, 0, 0, 1, 0, 1, 0, 1, 1];
 
 let i=0; j=0;
 while(i<arr.length){
@@ -139,7 +139,24 @@ while(i<arr.length){
 }
 i++;
 }
-console.log(arr);
+console.log(arr);*/
+//Question=> 6
+let arr = [5, 1, -17, -5, 19, 7, 2, -15, 3];
+
+let j = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+        // Swap arr[i] with arr[j]
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        j++;
+    }
+}
+
+console.log(arr); 
+
+
 
 
 
