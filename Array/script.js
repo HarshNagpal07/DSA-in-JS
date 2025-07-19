c4ef8cffbd0aa42f323a7c77a1956f2daabaa90a
Harsh056-star <hnagpal056@gmail.com>
2025-07-19 22:44:08 +0530

    update

--- Array/script.js
+++ Array/script.js
@@ -188,4 +188,43 @@ for(let j=1;j<=3;j++){
 }
 }*/
 
+//Move the element left side by 2
+/*let arr=[1, 2, 3, 4, 5];//5 element of array
 
+let k = Number(prompt("Enter k value"));
+k=k % arr.length;//This is for value is greater than 5 let's check
+let count = 0;
+for(let j=0;j<k;j++){
+    count++;
+    let copy = arr[0];
+    for(let i=0;i<arr.length-1;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[arr.length-1]=copy;
+}
+console.log(arr)
+console.log(count);*/
+
+//Move the element right side by 2
+/*let arr=[1, 2, 3, 4, 5];
+
+let k=Number(prompt("Enter k value"));
+k = k % arr.length  ;
+for(let j=0;j<k;j++){
+    let copy=arr[arr.length-1];
+    for(let i=arr.length-1;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    arr[0]=copy;
+}
+console.log(arr);*/
+//New method of left rotation
+let arr=[1,2,3,4,5]
+let temp= new Array(arr.length);
+let k= Number(prompt("Enter a value"))
+k = k % arr.length;
+
+for(let i=0;i<arr.length;i++){
+    temp[(i + k) % arr.length] = arr[i];
+}
+console.log(temp);
