3ccea31d24eab9a52a82807acc69959ca86da7e0
Harsh056-star <hnagpal056@gmail.com>
2025-07-20 18:11:31 +0530

    Remove Duplicate from sorted array

--- Array/script.js
+++ Array/script.js
@@ -269,3 +269,17 @@ function reverse(i,j){
         j--;
     }
 }*/
+
+//Remove duplicate from Sorted array(LeetCode Question)
+//Answer=>
+/*var RemoveDuplicate = Function(nums)
+{
+    let j = 1;
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i] != nums[i+1]){
+            nums[j] = nums[i+1];
+            j++;
+        }
+    }
+    return j;
+};*/
