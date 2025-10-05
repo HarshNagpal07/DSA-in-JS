2589aeb09e57f8b061dede15bd30aac83bbb38fc
Harsh056-star <hnagpal056@gmail.com>
2025-10-05 19:25:24 +0530

    Lucky Number

---
+++ Maps and Set/Lucky number.js
@@ -0,0 +1,17 @@
+//Largest Lucky Number in an Array
+let arr=[1,2,4,3,5,4,3,3,2,4,4];
+let max=-1;
+
+for(let i=0;i<arr.length;i++){
+    let count=0;
+    
+    for(let j=0;j<arr.length;j++){
+        if(arr[i]===arr[j]){
+            count++;
+        }
+    }
+    if(arr[i]===count){
+        max = Math.max(max,arr[i]);
+    }
+}
+console.log(max);
