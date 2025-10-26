a4903d6756fe9598e9e939fd1a758e4404159950
Harsh056-star <hnagpal056@gmail.com>
2025-10-26 13:28:55 +0530

    update

---
+++ Arrays and Strings/ArrayConcatenation.js
@@ -0,0 +1,11 @@
+let arr1=[5,41,9,5,2,7];
+let arr2=[4,5,3,8];
+
+let result=[];
+for(let i=0;i<arr1.length;i++){
+    result.push(arr1[i]);
+}
+for(let i=0;i<arr2.length;i++){
+    result.push(arr2[i]);
+}
+console.log("Result="+result);
