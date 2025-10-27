f4d7f2e1bfe7aa4534da60455f35ddae8e654be8
Harsh056-star <hnagpal056@gmail.com>
2025-10-27 17:01:34 +0530

    Update

--- Arrays and Strings/Maxin2DArray.js
+++ Arrays and Strings/Maxin2DArray.js
@@ -1,1 +1,13 @@
-let arr [[1,2,3],[3,2,1]]
+let arr = [[1,2,3],[3,2,1],[4,5,6]];
+
+let max=0;
+for(let i=0;i<arr.length;i++){
+    let sum=0;
+    for(let j=0;j<arr[i].length;j++){
+        sum=sum+arr[i][j];
+        if(sum>max){
+            max=sum;
+        }
+    }
+}
+console.log(max);
