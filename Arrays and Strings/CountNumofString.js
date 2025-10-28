567e607577a1921e098b992b14012e8c1fc90969
Harsh056-star <hnagpal056@gmail.com>
2025-10-28 23:23:39 +0530

    Update

--- Arrays and Strings/CountNumofString.js
+++ Arrays and Strings/CountNumofString.js
@@ -14,3 +14,16 @@
 let arr= ["alice and bob love leetcode", "i think so too", "this is great thanks very much"];
 
 let max=0;
+for(let i=0;i<arr.length;i++){
+    let count =1;
+    for(let j=0;j<arr.length;j++){
+        if(arr[i][j]==""){
+            count++;
+        }
+        console.log(count);
+    }
+    if(max<count){
+        max=count;
+    }
+}
+console.log(max);
