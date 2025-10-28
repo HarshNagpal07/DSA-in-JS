8a53156c436357a2b8428bdea046d730d8e44361
Harsh056-star <hnagpal056@gmail.com>
2025-10-28 23:05:31 +0530

    update

--- Arrays and Strings/CountNumofString.js
+++ Arrays and Strings/CountNumofString.js
@@ -1,12 +1,15 @@
-let arr="Tomorrow";
+// let arr="Tomorrow";
 
-for(let i=0;i<arr.length;i++){
-    let num=0;
-    if(arr.indexOf(arr[i]==i)){
-        for(let j=0;j<arr.length;j++){
-            if(arr[i]=== arr[j])
-            num++;
-        }
-        console.log(arr[i]+":"+num);
-    }
-}
+// for(let i=0;i<arr.length;i++){
+//     let num=0;
+//     if(arr.indexOf(arr[i]==i)){
+//         for(let j=0;j<arr.length;j++){
+//             if(arr[i]=== arr[j])
+//             num++;
+//         }
+//         console.log(arr[i]+":"+num);
+//     }
+// }
+//Max num in a sentence
+let arr= ["alice and bob love leetcode", "i think so too", "this is great thanks very much"];
+
