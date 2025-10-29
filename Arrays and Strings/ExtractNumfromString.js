ac4f830f99cd25297f2b2f01ed89fbbcd386eaab
Harsh056-star <hnagpal056@gmail.com>
2025-10-29 12:39:24 +0530

    Update

--- Arrays and Strings/ExtractNumfromString.js
+++ Arrays and Strings/ExtractNumfromString.js
@@ -13,3 +13,16 @@ for(let i=0;i<x.length;){
         i++;
     }
 }
+//Simpler way
+/*let x = "abc334v44d";
+
+for (let i = 0; i < x.length; i++) {
+  if (x.charCodeAt(i) >= 48 && x.charCodeAt(i) <= 57) {
+    let j = i;
+    while (j < x.length && x.charCodeAt(j) >= 48 && x.charCodeAt(j) <= 57) {
+      j++;
+    }
+    console.log(x.substring(i, j)); // full number
+    i = j - 1; // move i to the last digit of this number
+  }
+}*/
