21cd3a1a952a06884cfc19b1847e7d0e01cc492b
Harsh056-star <hnagpal056@gmail.com>
2025-10-29 12:19:40 +0530

    update

--- Arrays and Strings/ExtractNumfromString.js
+++ Arrays and Strings/ExtractNumfromString.js
@@ -1,10 +1,15 @@
 let x="abc334v44d";
 
-for(let i=0;i<x.length;i++){
+for(let i=0;i<x.length;){
     if(x.charCodeAt(i)>=48 && x.charCodeAt(i)<=57){
-        for(let j=i;j<x.length && x.charCodeAt(j)>=48 && x.charCodeAt(j)<=57;j++){
-            console.log(x.substring(i,j));
-            i=j;
-        }
+       let j=i;
+       while(j<x.length && x.charCodeAt(j)>=48 && x.charCodeAt(j)<=57){
+        j++;
+       }
+       console.log(x.substring(i,j));
+       i=j;
+    }
+    else{
+        i++;
     }
 }
