6453752d2cb097ac3d482f1d2b1e9058d1478e9f
Harsh056-star <hnagpal056@gmail.com>
2025-10-29 12:46:52 +0530

    update

--- Arrays and Strings/ArrangeNum.js
+++ Arrays and Strings/ArrangeNum.js
@@ -16,3 +16,22 @@ for(let i=0;i<arr.length;i++){
     }
 }
 console.log(ARR);
+//Using Push
+/*let arr = [1, -1, 3, 2, -7, -5, 11, 6];
+let ARR = [];
+
+// First push positive numbers
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] >= 0) {
+    ARR.push(arr[i]);
+  }
+}
+
+// Then push negative numbers
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 0) {
+    ARR.push(arr[i]);
+  }
+}
+
+console.log(ARR);*/
