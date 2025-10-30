177cfe3cde597138d3aae1bbfeac39f54d5bf26c
Harsh056-star <hnagpal056@gmail.com>
2025-10-30 12:25:29 +0530

    Update

--- Maps and Set/JewelsnStones.js
+++ Maps and Set/JewelsnStones.js
@@ -11,3 +11,17 @@
 //     }
 // }
 // console.log(count)
+//WIth maps
+let jewels="aAz";
+let stones="aAAbbbb";
+
+let max=new Set();
+let count=0;
+for(let i=0;i<jewels.length;i++){
+    max.add(jewels[i])
+}
+for(let i=0;i<stones.length;i++){
+    if(max.has(stones[i]))
+        count++;
+}
+console.log(count);
