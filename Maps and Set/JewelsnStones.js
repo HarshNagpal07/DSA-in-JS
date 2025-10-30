697716fc08dea90327f88a2348229d1e4b5babdc
Harsh056-star <hnagpal056@gmail.com>
2025-10-30 12:17:40 +0530

    Update

---
+++ Maps and Set/JewelsnStones.js
@@ -0,0 +1,13 @@
+// let jewels="aAz";
+// let stones="aAAbbbb";
+
+// let count=0;
+// for(let i=0;i<=stones.length;i++){
+//     for(let j=0;j<jewels.length;j++){
+//         if(stones[i]==jewels[j]){
+//             count++;
+//             break;
+//         }
+//     }
+// }
+// console.log(count)
