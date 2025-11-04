9c2687fc09374d35f7e1728db3b1a0f6eedcffa2
Harsh056-star <hnagpal056@gmail.com>
2025-11-04 15:25:17 +0530

    update

--- Stack/Operation.js
+++ Stack/Operation.js
@@ -1,4 +1,16 @@
 /*Push Pop Peek IsEmpty Full*/
 class Stack{
-    
+    constructor(){
+        this.top=-1;
+        this.stack=[];
+    }
+    push(element){
+        this.top=this.top+1;
+        this.stack[this.top]=element;
+    }
 }
+x=new Stack()
+x.push(7)
+x.push(10)
+x.push(15)
+console.log(x.stack);
