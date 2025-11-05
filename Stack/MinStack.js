ef03f490fa8c487fdb4018e7823aa79e40414f1a
Harsh056-star <hnagpal056@gmail.com>
2025-11-05 18:56:02 +0530

    update

---
+++ Stack/MinStack.js
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val) {
+        this.stack.push(val);
+        if (
+            this.minStack.length === 0 ||
+            val <= this.minStack[this.minStack.length - 1]
+        ) {
+            this.minStack.push(val);
+        }
+    }
+
+    pop() {
+        const popped = this.stack.pop();
+        if (popped === this.minStack[this.minStack.length - 1]) {
+            this.minStack.pop();
+        }
+    }
+
+    top() {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin() {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
+// ---------- Test the class ----------
+const obj = new MinStack();
+
+obj.push(-2);
+obj.push(0);
+obj.push(-3);
+
+console.log("Min:", obj.getMin()); // ➜ -3
+
+obj.pop();
+
+console.log("Top:", obj.top());    // ➜ 0
+console.log("Min:", obj.getMin()); // ➜ -2
--- Stack/Operation.js
+++ Stack/Operation.js
@@ -33,16 +33,16 @@ console.log(x.peek());
 console.log(x.isEmpty());*/
 
 //Without using funtions or OOPS
-let x=[];
-x.push(1);
-x.push(3);
-x.push(2);
-console.log(x[x.length-1]);
-if(x.length===0){
-    console.log("empty");
-}
-x.pop()
-console.log(x);
+// let x=[];
+// x.push(1);
+// x.push(3);
+// x.push(2);
+// console.log(x[x.length-1]);
+// if(x.length===0){
+//     console.log("empty");
+// }
+// x.pop()
+// console.log(x);
 
 class Stack{
     constructor(size=5){
