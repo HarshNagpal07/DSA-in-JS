957175e8239992cb5d9777c02382f52b22ffe260
Harsh056-star <hnagpal056@gmail.com>
2025-12-11 15:34:30 +0530

    update

---
+++ Linked List/Display_the_entire_linkedlist.js
@@ -0,0 +1,24 @@
+class Node{
+    constructor(a)
+    {
+        this.value=a;
+        this.next=undefined;
+    }
+}
+insertion_start=function(head,value)
+{
+    new_node=new Node(value);
+    new_node.next=head;
+    return new_node;
+}
+display=function(head)
+{
+    temp=head;
+    while(temp!=null){
+        console.log(temp.value);
+        temp=temp.next;
+    }
+}
+head=new Node(5);
+head=insertion_start(head,10);
+display(head);
