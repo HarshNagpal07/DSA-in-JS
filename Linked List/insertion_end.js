33e95fc2b4cbba2d00192201a33aea0e826b3805
Harsh056-star <hnagpal056@gmail.com>
2025-12-11 19:24:16 +0530

    update

---
+++ Linked List/insertion_end.js
@@ -0,0 +1,36 @@
+class Node{
+    constructor(a)
+    {
+        this.value=a;
+        this.next=undefined;
+    }
+}
+insertion_start=function(head,value)
+{
+    new_node=new Node(value);
+    new_node.next=head;
+    return new_node;
+}
+insertion_end=function(head,value)
+{
+    new_node=new Node(value);
+    temp=head;
+    while(temp.next!=null){
+        temp=temp.next;
+    }
+    temp.next=new_node;
+}
+display=function(head)
+{
+    temp=head;
+    while(temp!=null){
+        console.log(temp.value);
+        temp=temp.next;
+    }
+}
+head=new Node(5);
+head=insertion_start(head,10);
+head=insertion_start(head,0);
+insertion_end(head,20);
+insertion_end(head,30);
+display(head);
