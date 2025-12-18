6acdb13461c4e46f5d0689b90f827851f77b9343
Harsh056-star <hnagpal056@gmail.com>
2025-12-18 12:37:35 +0530

    update

---
+++ Linked List/deletion_start.js
@@ -0,0 +1,50 @@
+ class Node{
+    constructor(a)
+    {
+        this.value=a;
+        this.next=undefined;
+    }
+}
+insertion_start=function(head,value)
+{
+    new_node=new Node(value);
+    new_node.next=head;
+    return new_node;
+}
+insertion_end=function(head,value)
+{
+    new_node=new Node(value);
+    temp=head;
+    while(temp.next!=null){
+        temp=temp.next;
+    }
+    temp.next=new_node;
+}
+insertion_middle=function(head,n,value){
+    new_node=new Node(value);
+    temp=head;
+    while(n-1>0){
+        temp=temp.next;
+        n=n-1;
+    }
+    x=temp.next;
+    temp.next=new_node;
+    new_node.next=x;
+}
+display=function(head)
+{
+    temp=head;
+    while(temp!=null){
+        console.log(temp.value);
+        temp=temp.next;
+    }
+}
+head=new Node(5);
+head=insertion_start(head,10);
+head=insertion_start(head,0);
+insertion_end(head,20);
+insertion_end(head,30);
+insertion_middle(head,3,2);
+head=head.next;//this is for the starting
+display(head);
+//This doesnot work
