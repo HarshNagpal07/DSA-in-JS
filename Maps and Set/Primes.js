3a7f6c9fb147b6d0ba858e582eeeb69a11063b74
Harsh056-star <hnagpal056@gmail.com>
2026-01-16 23:35:19 +0530

    update

--- Maps and Set/Primes.js
+++ Maps and Set/Primes.js
@@ -1,35 +1,35 @@
 //With maps
-// let n = 10; // you can change this value to test with other numbers
+let n = 10; // you can change this value to test with other numbers
 
-// if (n <= 2) {
-//     console.log(0);
-// } else {
-//     let isPrime = new Map();
+if (n <= 2) {
+    console.log(0);
+} else {
+    let isPrime = new Map();
 
-//     // Step 1: Initialize all numbers as prime
-//     for (let i = 0; i < n; i++) {
-//         isPrime.set(i, true);
-//     }
+    // Step 1: Initialize all numbers as prime
+    for (let i = 0; i < n; i++) {
+        isPrime.set(i, true);
+    }
 
-//     // Step 2: Mark non-primes
-//     for (let i = 2; i * i < n; i++) {
-//         if (isPrime.get(i)) {
-//             for (let j = i * i; j < n; j += i) {
-//                 isPrime.set(j, false);
-//             }
-//         }
-//     }
+    // Step 2: Mark non-primes
+    for (let i = 2; i * i < n; i++) {
+        if (isPrime.get(i)) {
+            for (let j = i * i; j < n; j += i) {
+                isPrime.set(j, false);
+            }
+        }
+    }
 
-//     // Step 3: Count primes
-//     let count = 0;
-//     for (let i = 2; i < n; i++) {
-//         if (isPrime.get(i)) {
-//             count++;
-//         }
-//     }
+    // Step 3: Count primes
+    let count = 0;
+    for (let i = 2; i < n; i++) {
+        if (isPrime.get(i)) {
+            count++;
+        }
+    }
 
-//     console.log(count);
-// }
+    console.log(count);
+}
 // With array app
 n=10;
      if (n <= 2)
